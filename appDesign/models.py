0b70a5ea5e152d9045a379337e935a0fd7898c29
Roma 12/10 <aon24@mail.ru>
2023-07-12 18:33:53 +0300

    q

--- appDesign/models.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*- 
-'''
-Created 2020
-
-@author: aon
-'''
-
-from common.dbToolkit.DC import DC
-Page = DC({'_meta' : DC()})
--- imidjan.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*- 
-"""
-AON 2021
-
-"""
-
-def HttpResponse(bo, ty=None, st=200):
-    return (st, ty, bo)
-
